cbeb58f8143e11ff918c354ccefabd8fd7792a6a
LGD-P <nplgdjp@gmx.com>
2023-03-14 17:24:09 +0100

    async to fetch

--- js/the_best_movie_api.js
+++ js/the_best_movie_api.js
@@ -3,12 +3,42 @@ const best_movie_alone = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_sco
 const img = document.querySelector(".best-movie")
 const title = document.querySelector(".best-movie-title")
 
+/*
 fetch(best_movie_alone)
     .then(res =>  {return res.json();})
     .then(data =>  {img.src = data.results[0].image_url});
-    
-    
+ */   
+
+/*
 fetch(best_movie_alone)
-    .then(res => res.json())    
-    .then(test => title.replaceChildren(test.results[0].title))
+.then(res => res.json())    
+.then(test => title.replaceChildren(test.results[0].title))
+*/
+
+/*
+*This function replace best movie img in main page 
+*/
+
+async function display_best_movie_img (img) {
+    const reponse = await fetch(best_movie_alone);
+    const json = await reponse.json();
+    const data = await json.results[0].image_url ;
+    return img.src = data;
+};
     
+display_best_movie_img(img);
+
+
+/*
+*This function put best movie title in main page 
+*/
+
+async function display_best_movie_title (title) {
+    const reponse = await fetch(best_movie_alone);
+    const json = await reponse.json();
+    const data = await json.results[0].title;
+    return title.replaceChildren(data);
+};
+
+display_best_movie_title(title);
+
